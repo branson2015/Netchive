d616734b5cb9e690446acb490995dd3fe8ec789b
px1624 <33167430+px1624@users.noreply.github.com>
2018-09-29 13:48:44 -0400

    modified popup

--- ChromeExt/background.js
+++ ChromeExt/background.js
@@ -1,6 +1,3 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 chrome.browserAction.setBadgeText({text: 'ON'});
-  chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
+chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
