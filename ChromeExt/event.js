33f3d96bc80ebb77b13890759fcc63603212cfd4
px1624 <33167430+px1624@users.noreply.github.com>
2018-09-29 20:48:14 -0400

    Error handling for chrome://

--- ChromeExt/event.js
+++ ChromeExt/event.js
@@ -1,6 +1,13 @@
 function getPageDetails(callback) {
     // Inject the content script into the current page
-    chrome.tabs.executeScript({ file: 'content.js' });
+    chrome.tabs.executeScript({ file: 'content.js' }, function(){
+        if (chrome.runtime.lastError) {
+            var errorMsg = chrome.runtime.lastError
+            if (errorMsg == "Cannot access a chrome:// URL") {
+                console.log("Chrome:// is not supported");
+            }
+         }
+    });
     // When a message is received from the content script
     chrome.runtime.onMessage.addListener(function(message) {
         // Call the callback function
